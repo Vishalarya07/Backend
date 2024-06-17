f0ec5cfc2f3f587eeb4ed10eef8374701d0ae921
Vishalarya07 <aryav7196@gmail.com>
2024-06-17 06:37:56 +0530

    DataBase Connection

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        console.log("MongoDB is Connected host:${connectionInstance.connection.host}" );
+        console.log(connectionInstance);
+    } catch (error) {
+        console.log("DataBase is not connected", error);
+        throw error;
+    }
+}
+
+export default connectDB;
